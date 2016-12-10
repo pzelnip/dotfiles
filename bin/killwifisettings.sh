f449e3996ae03aff602238c081266d3b7bddab8d
Adam Parkin <pzelnip@gmail.com>
2016-12-10 14:08:33 -0800

    Added script for resetting wifi stuff on osx

---
+++ bin/killwifisettings.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -rf /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist
+rm -rf /Library/Preferences/SystemConfiguration/com.apple.network.identification.plist
+rm -rf /Library/Preferences/SystemConfiguration/NetworkInterfaces.plist
+rm -rf /Library/Preferences/SystemConfiguration/preferences.plist
