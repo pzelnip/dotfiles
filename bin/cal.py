bcb094cd17d9276d1953a8965eab3eea84b81266
Adam Parkin <pzelnip@gmail.com>
2021-03-05 18:36:34 -0800

    Blacken cal.py

--- bin/cal.py
+++ bin/cal.py
@@ -8,7 +8,20 @@ from datetime import datetime
 def main():
     cur_year = datetime.now().year
     query = sys.argv[1] if len(sys.argv) > 1 else str(cur_year)
-    months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+    months = [
+        "january",
+        "february",
+        "march",
+        "april",
+        "may",
+        "june",
+        "july",
+        "august",
+        "september",
+        "october",
+        "november",
+        "december",
+    ]
     month_mapping = {month: i + 1 for i, month in enumerate(months)}
     for month, idx in month_mapping.items():
         if query.lower() in month:
@@ -20,6 +33,7 @@ def main():
     if len(query) == 4 and query.isdigit():
         print(calendar.calendar(int(query)))
         return
+
 
 if __name__ == "__main__":
     exit(main())
